faea01b76f9e06689c5d153ff5d12aea6f457735
Alex Zhang <alex@isitesoftware.com>
2013-07-17 14:30:55 -0700

    optimize

--- hello.asm
+++ hello.asm
@@ -1,5 +1,6 @@
 ;equivalent to what tee does, should investigate fsync and close
 ;all registers besides eax are preserved
+%define BUFFER_SIZE 1024
 section .text
 	global main
 	main:
@@ -12,41 +13,26 @@ section .text
 	int 80h ; call kernel
 	
 	mov ebx, eax; move file descriptor from result of open to ebx
+	mov [ifd], eax ; set the input file descriptor
 	mov eax, 19 ; lseek
 	mov ecx, 0 ; offset
 	mov edx, 2; seek_end
 	int 80h
-	mov [filesize], eax;eax is now the file size
+	mov edi, eax ;eax is now the file size
 	
 	mov eax, 19 ; lseek
 	mov ecx, 0 ; offset
 	mov edx, 0; seek_set (seek back to beginning for read)
 	int 80h
-	;mov eax, [filesize]
-	;idiv [bufsize] ; eax has quotient, remainder is edx
-	;cmp edx, 0
-	;je noadd
-	;add eax, 1
-	;noadd:
-	mov eax, 3 ; read
-	mov ecx, buf
-	mov edx, [bufsize]
-	int 80h
-	push eax ; we need to save this i guess
-	
-	
-	
-	;ebx _should_ be the same
-	;mov ecx, stat
-	;mov eax, 28 ; fstat
-	;int 80h 
-	
-	pop eax
-	mov edx, eax ; length written
-	mov [size], edx ;save length of buffer for later
-	mov eax, 4 ;write 
-	mov ebx, 1 ; stdout
-	int 80h
+
+	mov eax, edi
+	mov ebx, BUFFER_SIZE; can't directly divide with constant
+	idiv ebx ; eax has quotient, remainder is edx
+	cmp edx, 0
+	je noadd
+	add eax, 1
+	noadd:
+	mov edi, eax
 
 	pop ebx ;argv[2]
 	mov eax, 5 ; open
@@ -54,24 +40,39 @@ section .text
 	mov edx, 700q ;file permission
 	int 80h 
 
-	mov ebx, eax ;move file descriptor
-	mov edx, [size] ; restore size written
+	mov [ofd], eax
+
+	loop:
+	mov eax, 3 ; read
+	mov ebx, [ifd]
+	mov ecx, buf
+
+	mov edx, BUFFER_SIZE
+	int 80h	
+		
+	mov edx, eax ; length read
+	mov esi, edx ;save length of buffer for later
+	mov eax, 4 ;write 
+	mov ebx, 1 ; stdout
+	int 80h
+
+	mov ebx, [ofd] ;restore file descriptor
+	mov edx, esi; restore size written
 	mov ecx, buf ; write the buffer
-;	mov ebx, 1
 	mov eax, 4 ; write
 	int 80h
+
+	sub edi, 1
+	cmp edi, 0
+	jne loop
 
 	mov ebx, 0 ; exit code
 	mov eax, 1 ; exit
 	int 80h
 
-section .data
-	msg db "hello world", 10
-	len equ $ - msg
-	bufsize dd 1024
-	mode equ 2q | 100q
-	size dd 2100786140
-	filesize dd 0 
+section .data	
+	mode equ 2q | 100q		
+	ifd dd 0
+	ofd dd 0
 section .bss
-	buf resb 1024
-	stat resb 52
+	buf resb BUFFER_SIZE	
